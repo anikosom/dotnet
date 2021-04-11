f589761fd26508b2008f76fff98318a05bafc271
anyan <anikosom88@gmail.com>
2021-04-11 22:37:54 +0300

    Доменные модели

---
+++ ToDoWebApplication/Domain/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoWebApplication.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Title must contain at least two characters!")]
+        [MaxLength(200, ErrorMessage = "Title must contain a maximum of 200 characters!")]
+        public string Title { get; set; }
+
+        public byte Priority { get; set; }
+    }
+}
---
+++ ToDoWebApplication/Domain/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoWebApplication.Domain
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Title must contain at least two characters!")]
+        [MaxLength(200, ErrorMessage = "Title must contain a maximum of 200 characters!")]
+        public string Title { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public int? CategoryId { get; set; }
+    }
+}
